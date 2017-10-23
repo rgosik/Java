542c726fe7e025cb84274fdf79faa4edb7526f58
rgosik <grafal96@gmail.com>
2017-10-23 11:00:04 +0200

    Add files via upload

---
+++ jdbcdemo-master/src/main/java/com/example/jdbcdemo/service/PersonManager.java
@@ -0,0 +1,12 @@
+package com.example.jdbcdemo.service;
+
+import java.util.List;
+
+import com.example.jdbcdemo.domain.Person;
+
+public interface PersonManager {
+	
+	public int addPerson(Person person);
+	public List<Person> getAllPersons();
+
+}
